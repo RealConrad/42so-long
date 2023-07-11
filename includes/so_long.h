58954bd3a9f957a003486d3f20dd33cfa062e4cd
cwenz <wenzconrad@gmail.com>
2023-07-11 18:40:50 +0200

    added header file

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,13 +6,49 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:57:51 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/10 14:40:12 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/11 18:38:45 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../libraries/42c-library/libft_main.h" 
+# include <stdbool.h>
+# include <stdlib.h>
+
+/* Include libraries */
+# include "../libraries/42c-library/c_library.h" 
+# include "../libraries/MLX42/include/MLX42/MLX42.h"
+
+
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
+
+# define WIDTH 1000
+# define HEIGHT 1000
+
+# define MOVE_SPEED 50
+
+typedef struct s_player {
+	mlx_texture_t	*sprite;
+	mlx_image_t		*img;
+	int				curr_frame;
+} t_player;
+
+typedef struct s_game {
+	mlx_t			*mlx;
+	t_player		*player;
+}	t_game;
+
+/* Initialize */
+void	init_game(t_game *game_object);
+
+/* Handle Input */
+void	handle_input(mlx_key_data_t keydata, void *param);
+bool	is_movement(mlx_key_data_t keydata);
+void	count_moves();
+
+/* Handle errors */
+void	cleanup_and_exit();
 
 #endif /* SO_LONG_H */
