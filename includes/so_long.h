527261a8a7ed57f9cbfc308bd1b09e0b7483968c
cwenz <wenzconrad@gmail.com>
2023-07-13 20:50:36 +0200

    updated structs and added enum

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:57:51 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/11 18:38:45 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/13 20:11:53 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,26 @@
 # include "../libraries/42c-library/c_library.h" 
 # include "../libraries/MLX42/include/MLX42/MLX42.h"
 
+/* Player */
+# define PLAYER_SPRITE_PATH "./assets/player/HeroKnight_Idle_"
+# define MAX_SPRITES 7
+# define MOVE_SPEED 50
 
-# define EXIT_SUCCESS 0
-# define EXIT_FAILURE 1
+# define FILENAME_SIZE 50
 
+/* Game dimensions */
 # define WIDTH 1000
 # define HEIGHT 1000
 
-# define MOVE_SPEED 50
+typedef enum e_exit_type {
+	SUCCESS,
+	FAIL,
+}	t_exit_type;
 
 typedef struct s_player {
-	mlx_texture_t	*sprite;
-	mlx_image_t		*img;
-	int				curr_frame;
+	mlx_texture_t	**sprites; // Array of sprites, one for each frame
+	mlx_image_t		*img; // current image
+	int				curr_frame; // index of the current frame
 } t_player;
 
 typedef struct s_game {
@@ -42,6 +49,10 @@ typedef struct s_game {
 
 /* Initialize */
 void	init_game(t_game *game_object);
+char	*get_player_sprites(int	sprite_index);
+
+/* Player */
+void	loop_player_idle_animation(void *param);
 
 /* Handle Input */
 void	handle_input(mlx_key_data_t keydata, void *param);
