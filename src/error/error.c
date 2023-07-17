2f71e43285e128af886dcf4a4e014716c3c56b65
cwenz <wenzconrad@gmail.com>
2023-07-17 18:03:21 +0200

    added custom error message for error.c

--- src/error/error.c
+++ src/error/error.c
@@ -6,14 +6,14 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 17:31:49 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/13 19:47:43 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/17 16:49:22 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "so_long.h"
 
-void	cleanup_and_exit(t_exit_type exit_type)
+void	cleanup_and_exit(t_exit_type exit_type, char *error_msg)
 {
-	ft_printf("Error");
+	ft_printf("Error:\n%s\n", error_msg);
 	exit(exit_type);
 }
