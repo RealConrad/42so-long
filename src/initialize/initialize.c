bfb74d1619801f05c72f171e5bc59eaac17b5d2e
cwenz <wenzconrad@gmail.com>
2023-07-25 13:35:03 +0200

    added frame skip amount

--- src/initialize/initialize.c
+++ src/initialize/initialize.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 15:56:56 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/24 17:22:56 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/25 13:33:14 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,13 @@ void	init_game(t_game *game_object, char *map_name)
 	if (!game_object->mlx)
 		cleanup_and_exit(FAIL, "Failed to initialize mlx.");
 	init_map(game_object, map_name);
+	ft_printf("%d\n", game_object->map->num_collectables);
 	init_hooks(game_object);
-}
-
-void player(void *param)
-{
-	t_game *game_object;
-
-	game_object = (t_game *)param;
-	animate_sprite(game_object, game_object->player);
-}
-
-void collectable(void *param)
-{
-	t_game *game_object;
-
-	game_object = (t_game *)param;
-	animate_sprite(game_object, game_object->map->collectables->animation);
 }
 
 static void init_hooks(t_game *game_object)
 {
-	// mlx_loop_hook(game_object->mlx, &init_animation, game_object);
-	mlx_loop_hook(game_object->mlx, &player, game_object);
-	// mlx_loop_hook(game_object->mlx, &collectable, game_object);
-	// animate_sprite(game_object, game_object->player);
-	// animate_sprite(game_object, game_object->map->collectables->animation);
+	mlx_loop_hook(game_object->mlx, &init_animation, game_object);
 	mlx_key_hook(game_object->mlx, &handle_input, game_object);
 }
 
