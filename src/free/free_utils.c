70bce33a7c9a63fecd679021352ad6016e03cde9
cwenz <wenzconrad@gmail.com>
2023-07-30 15:40:58 +0200

    added functions to free memory

---
+++ src/free/free_utils.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/30 15:01:47 by cwenz             #+#    #+#             */
+/*   Updated: 2023/07/30 15:19:18 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "so_long.h"
+
+void	free_textures(mlx_texture_t **sprite_textures)
+{
+	int	i;
+
+	i = 0;
+	while (sprite_textures[i])
+	{
+		mlx_delete_texture(sprite_textures[i]);
+		i++;
+	}
+	free(sprite_textures);
+}
+
+void	free_images(t_game *game_object, mlx_image_t **sprite_images)
+{
+	int	i;
+
+	i = 0;
+	while (sprite_images[i])
+	{
+		mlx_delete_image(game_object->mlx, sprite_images[i]);
+		i++;
+	}
+	free(sprite_images);
+}
