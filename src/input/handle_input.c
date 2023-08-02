b2db855bc738d14052624404c69b38ca0c988d0a
cwenz <wenzconrad@gmail.com>
2023-08-02 13:24:19 +0200

    added fix for player movement count, counting double when moving 1 tile

--- src/input/handle_input.c
+++ src/input/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:34:22 by cwenz             #+#    #+#             */
-/*   Updated: 2023/08/01 16:36:02 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/08/02 13:20:44 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@ void	handle_input(mlx_key_data_t keydata, void *param)
 	t_game	*game_object;
 
 	game_object = (t_game *)param;
-	if (is_movement(keydata))
+	if (is_movement(keydata)
+		&& (keydata.action == MLX_PRESS
+			|| keydata.action == MLX_REPEAT))
 		handle_movement(game_object);
 	else if (mlx_is_key_down(game_object->mlx, MLX_KEY_ESCAPE))
-		cleanup_and_exit(game_object, SUCCESS, "");
+		display_game_over(game_object);
 }
 
 static void	handle_movement(t_game *game_object)
@@ -40,14 +42,15 @@ static void	handle_movement(t_game *game_object)
 		delta_x = -1;
 	else if (mlx_is_key_down(game_object->mlx, MLX_KEY_D))
 		delta_x = 1;
-	
+
 	tile_x = game_object->player->x + delta_x;
 	tile_y = game_object->player->y + delta_y;
-	
+
 	if (game_object->map->map_plan[tile_y][tile_x] != WALL)
 	{
 		game_object->player->x = tile_x;
 		game_object->player->y = tile_y;
-		count_moves();
+		game_object->hud->has_player_moved = true;
+		game_object->hud->num_player_moves++;
 	}
 }
